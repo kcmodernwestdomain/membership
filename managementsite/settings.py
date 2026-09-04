58a62ed13d58cdab5971a5c67f07679dd8015c82
kcmodernwest <kcmodernwestdomain@proton.me>
2026-09-04 04:52:44 -0700

    cores update

--- managementsite/settings.py
+++ managementsite/settings.py
@@ -158,10 +158,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://www.columbiarc.com",
     "https://www.kcmodernwest.com",
+    "https://www.kcmodernwest.com",
+
 ]
